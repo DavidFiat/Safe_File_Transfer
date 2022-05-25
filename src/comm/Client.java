17d501d8bf59b06b15d03b879b8bb9fab1131a88
Mateo <mateo.loaiza1@correo.icesi.edu.co>
2022-05-25 08:31:46 -0500

    Almost done with the file transfer, need to check the is.readAllBytes() method and change it

--- src/comm/Client.java
+++ src/comm/Client.java
@@ -16,19 +16,21 @@ import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 
+
 public class Client {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-
         try {
 
-            String path = "D:\\Documents\\Quinto Semestre\\ProRed\\Test\\Enviar\\java.pdf";
+            //Pedir el path del archivo
+            String path = "D:\\Documents\\Octavo Semestre\\Ciberseguridad\\Workspace\\Mensaje.txt";
             File file = new File(path);
-            String ab = file.getAbsolutePath();
-            FileInputStream fis = new FileInputStream(file);
+//            String ab = file.getAbsolutePath();
+//            FileInputStream fis = new FileInputStream(file);
 
+            //Empezando la conexion TCP
             System.out.println("Enviando solicitud...");
 
             //Socket es la puerta de conexion o comunicacion
@@ -40,41 +42,40 @@ public class Client {
 
             System.out.println("Conectados");
 
-            File publicKeyFile = new File("publicKey");
-            byte[] publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
+            System.out.println("HOLA");
+            //Recibimos la clave publica
+            InputStream is = socket.getInputStream();
+            //Necesitamos cambiar esto para leer solo los Bytes de la clave
+            byte[] publicKeyBytes = is.readAllBytes();
+            System.out.println("Lo recibo");
 
+            //Recuperamos la instancia de la clave publica
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
             PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
 
+            //Ciframos la informacion del archivo
             byte[] fileBytes = Files.readAllBytes(Path.of(file.getPath()));
 
             Cipher encryptCipher = Cipher.getInstance("RSA");
             encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
             byte[] encryptedFileBytes = encryptCipher.doFinal(fileBytes);
 
+            //Mandamos el archivo cifrado
+            FileOutputStream stream = new FileOutputStream("EncryptedFile");
+            stream.write(encryptedFileBytes);
+            OutputStream os = socket.getOutputStream();
+            os.write(encryptedFileBytes);
 
 
-//            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            String publicKey = in.readLine();
-//            System.out.println("Publica: "+publicKey);
+            is.close();
+            stream.close();
 
 
 
-        } catch (IOException e) {
+
+        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
             e.printStackTrace();
         }
     }
--- src/comm/Server.java
+++ src/comm/Server.java
@@ -1,8 +1,13 @@
 package comm;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
 import java.security.*;
 
 public class Server {
@@ -18,6 +23,7 @@ public class Server {
             Socket socket = server.accept();
             System.out.println("Conectado");
 
+            //Con esto generamos el par de claves RSA
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
             keyPairGenerator.initialize(1024);
             KeyPair keyPair = keyPairGenerator.generateKeyPair();
@@ -25,34 +31,44 @@ public class Server {
             PublicKey publicKey = keyPair.getPublic();
             PrivateKey privateKey = keyPair.getPrivate();
 
+
+            //Grabamos la clave pública a un archivo
             try (FileOutputStream fos = new FileOutputStream("publicKey")) {
                 fos.write(publicKey.getEncoded());
             }
+            //Grabamos la clave privada a un archivo
+            try (FileOutputStream fos = new FileOutputStream("privateKey")) {
+                fos.write(privateKey.getEncoded());
+            }
 
-            PrintWriter out = new PrintWriter(socket.getOutputStream());
-            out.println(publicKey.toString());
-            out.flush();
+            //Mandamos la clave publica al cliente
+            OutputStream os = socket.getOutputStream();
+            os.write(publicKey.getEncoded());
+            System.out.println("Lo mando");
+            os.flush();
 
-//            OutputStream os = socket.getOutputStream();
-//
-//
-//            byte[] buffer = new byte[128];
-//            int readBytes = 0;
-//
-//
-//            while((readBytes = fis.read(buffer)) != -1) {
-//                System.out.println(readBytes);
-//                os.write(buffer, 0, readBytes);
-//
-//            }
-//            os.close();
+            //Obtenemos la informacion del archivo
+            InputStream is = socket.getInputStream();
+            byte[] encryptedFileBytes = is.readAllBytes();
 
-            System.out.println("Publica: "+ publicKey);
-            System.out.println("Privada: "+ privateKey);
+            //Usamos Cipher para descifrar el archivo
+            Cipher decryptCipher = Cipher.getInstance("RSA");
+            decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] decryptedFileBytes = decryptCipher.doFinal(encryptedFileBytes);
+
+            String recievedPath = "D:\\Documents\\Octavo Semestre\\Recibidos\\Mensaje";
+            try (FileOutputStream fos = new FileOutputStream(recievedPath)) {
+                fos.write(decryptedFileBytes);
+                System.out.println("EXITO");
+            }
+
+            os.close();
+//            System.out.println("Publica: "+ publicKey);
+//            System.out.println("Privada: "+ privateKey);
 
 //            while(true) {}
 
-        } catch (IOException | NoSuchAlgorithmException e) {
+        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
